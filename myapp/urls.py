bfaeb6a403f1d7f5deae835f44c6f23ecbfdbfaf
11056045黃思璇 <11056045@ntub.edu.tw>
2025-03-03 21:41:28 +0800

    123

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,15 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('tables/', views.tables, name='tables'),
-    path('register/', views.register, name='register'),
-    path('password/', views.password, name='password'),
-    path('login/', views.login, name='login'),
-    path('layout-static/', views.layout_static, name='layout-static'),
-    path('layout-sidenav-light/', views.layout_sidenav_light, name='layout-sidenav-light'),
-    path('charts/', views.charts, name='charts'),
-    path('500/', views.error_500, name='500'),
-    path('401/', views.error_401, name='401'),
-    path('404/', views.error_404, name='404'),
+    path('index.html', views.index, name='index'),
+    # path('index.html', views.index, name='index')
+    # 對,我也不知道有時換這個反而能跑,有時又不能跑    
+    path('tables.html', views.tables, name='tables'),
+    path('register.html', views.register, name='register'),
+    path('password.html', views.password, name='password'),
+    path('login.html', views.login, name='login'),
+    path('layout-static.html', views.layout_static, name='layout-static'),
+    path('layout-sidenav-light.html', views.layout_sidenav_light, name='layout-sidenav-light'),
+    path('charts.html', views.charts, name='charts'),
+    path('500.html', views.error_500, name='500'),
+    path('401.html', views.error_401, name='401'),
+    path('404.html', views.error_404, name='404'),
 ]
+
+# 原本沒有這些路徑
