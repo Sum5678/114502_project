7b6315eebff72aff6abe1b8649d579e28addb2ef
11056035陳雨琦 <11056035@ntub.edu.tw>
2025-03-31 11:20:07 +0800

    增urls.py views.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -20,7 +20,10 @@ urlpatterns = [
     path('404.html', views.error_404, name='404'),
     path('harassment_prevention/', views.harassment_prevention, name='harassment_prevention'),
     path('education/', views.education_page, name='education_page'),
-    path('police/', views.nearest_police, name='nearest_police')
+    path('police/', views.nearest_police, name='nearest_police'),
+    path('anonymous-chat/', views.anonymous_chat, name='anonymous-chat'),  # anonymous-chat 頁面
+    path('index35/', views.index35, name='index35'),  # 新增 index35.html 路由
+    path('map/', views.map_view, name='map'),  # 添加 map.html 路由
 ]
 
 # 原本沒有這些路徑
