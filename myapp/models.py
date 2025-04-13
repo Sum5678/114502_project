6d2059fa4cfd2a6948b5bdda9dc825864b60ae61
11056042 <11056042@ntub.edu.tw>
2025-04-13 20:23:44 +0800

    最近警局-2

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 
 # Create your models here.
+class TaiwanRegion(models.Model):
+    country_city = models.CharField(max_length=50)
+    district_town = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.country_city} - {self.district_town}"
