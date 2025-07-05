56f50af43875ed6ea4a2c206ca7b761ebcf2a340
11056035陳雨琦 <11056035@ntub.edu.tw>
2025-07-05 21:42:16 +0800

    已處理封面圖片縮小問題+模板顏色換成藍色系

--- myapp/static/assets/js/main.js
+++ myapp/static/assets/js/main.js
@@ -13,7 +13,7 @@
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
     if (!selectHeader || (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top'))) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    window.scrollY > 50 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
 
   document.addEventListener('scroll', toggleScrolled);
