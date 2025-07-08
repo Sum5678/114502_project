f822373a950f705126eb0f956983037e231fd681
11056045黃思璇 <11056045@ntub.edu.tw>
2025-07-09 04:10:18 +0800

    黃思璇的上傳

--- myapp/urls.py
+++ myapp/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path('harassment_prevention/', views.harassment_prevention, name='harassment_prevention'),
     path('education/', views.education_page, name='education_page'),
     path('nearest-police/', nearest_police_view, name='nearest_police'),
-    
+    path('api/submit_report/', views.submit_report, name='submit_report'),
+    path('api/submit_store/', views.submit_store, name='submit_store'),
+    path('business_upload/', views.business_upload_view, name='business_upload'),
     path('admin/', admin.site.urls),
     path('admin/region/', views.taiwan_regions_admin, name='taiwan_regions_admin'),
     path('taiwan-regions-admin/', views.taiwan_regions_admin, name='taiwan_regions_admin'),
@@ -97,11 +99,8 @@ urlpatterns = [
     path('<str:room_name>/', views.room, name='room'),
 
     # 其他路由
-    #思璇
-     #思璇
-    path('api/submit_report/', views.submit_report, name='submit_report'),
 
-
+   
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 原本沒有這些路徑
