4e31086d45f35ca85b9c6f1793d8bd52cc258b41
11056001 Sunny <11056001@ntub.edu.tw>
2025-07-18 04:01:38 +0800

    更改傳送表單後admin_id=9999(原本是1)

--- myapp/views.py
+++ myapp/views.py
@@ -965,7 +965,7 @@ def submit_report(request):
                 img_url=img_url,
                 time_created=timezone.now(),
                 review_status=review_status,
-                admin_id = 1
+                admin_id = 99999
             )
 
             return JsonResponse({"status": "success", "review_status": review_status})
