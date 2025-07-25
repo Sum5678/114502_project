f28947d0fb4f5d0e4dc30ab2a1a19f4be9490b81
11056001 Sunny <11056001@ntub.edu.tw>
2025-07-25 20:18:11 +0800

    商家地圖顯示部分

--- myapp/urls.py
+++ myapp/urls.py
@@ -113,6 +113,7 @@ urlpatterns = [
     path('api/approved-locations/', views.approved_locations_api, name='approved_locations_api'),#事件地圖
     path('store_map/', views.store_map_view, name='store_map'),#商家地圖
     path('api/stores/', views.store_data_api, name='store_data_api'),#商家地圖
+    
 
 
     # Pemap 管理員相關
