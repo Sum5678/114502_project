094cf71acb3afb50ad5d326e6b18c6f4d80fe80a
11056035陳雨琦 <11056035@ntub.edu.tw>
2025-08-03 04:22:44 +0800

    前端時間顯示正確

--- myproject/settings.py
+++ myproject/settings.py
@@ -143,9 +143,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'  # ✅ 設定為台灣時區
 
 USE_I18N = True
 
