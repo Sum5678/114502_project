51382522a9a01883783c2472695a9458773edf67
11056042 <11056042@ntub.edu.tw>
2025-08-04 18:04:12 +0800

    還有這個要刪

--- myapp/urls.py
+++ myapp/urls.py
@@ -61,7 +61,6 @@ urlpatterns = [
     path('autodial/', views.autodial_view, name='autodial'),
     path('mychatroom/', views.mychatroom, name='mychatroom'),
     path('community/', views.community, name='community'),
-    path('face_detection/', views.face_detection_view, name='face_detection'), #人臉辨識
     path('post/', views.post, name='post'),
     path('post_display/', views.post_display, name='post_display'),
     path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
