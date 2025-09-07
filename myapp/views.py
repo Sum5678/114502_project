6a94017db7c82a2db46cc5c6cd0546a5eec27ba2
11056001 Sunny <11056001@ntub.edu.tw>
2025-09-07 23:23:55 +0800

    商家廣告

--- myapp/views.py
+++ myapp/views.py
@@ -3888,6 +3888,7 @@ from .models import StoreAll, StoreAd, StoreAdImage
 from .forms import StoreAdForm
 import os
 
+
 def upload_store_ad(request):
     if request.method == 'POST':
         form = StoreAdForm(request.POST)
@@ -3901,19 +3902,26 @@ def upload_store_ad(request):
                 form.add_error('st_id', '找不到此商家編號')
                 return render(request, 'store_upload_ad.html', {'form': form})
 
-            # ✅ 直接取代 get_or_create
-            try:
-                ad = StoreAd.objects.get(st_id=store.st_id)
-            except StoreAd.DoesNotExist:
-                ad = StoreAd(st_id=store.st_id)
+            # 🚀 強制轉 int (因為 StoreAd.st_id 是 IntegerField)
+            store_ad_id = int(store.st_id)
 
-            ad.ad_content = form.cleaned_data['ad_content']
-            ad.ad_radius = form.cleaned_data['ad_radius']
-            ad.enabled = form.cleaned_data['enabled']
-            ad.save()
+            # ✅ 更新或新增廣告
+            ad, created = StoreAd.objects.update_or_create(
+                st_id=store_ad_id,
+                defaults={
+                    'ad_content': form.cleaned_data['ad_content'],
+                    'ad_radius': form.cleaned_data['ad_radius'],
+                    'enabled': form.cleaned_data['enabled'],
+                }
+            )
 
-            # 先刪掉舊圖片
-            ad.images.all().delete()
+            # 先刪掉舊的圖片
+            old_images = ad.images.all()
+            for img in old_images:
+                img_path = os.path.join(settings.BASE_DIR, img.image_url.strip("/"))
+                if os.path.exists(img_path):
+                    os.remove(img_path)
+                img.delete()
 
             # 儲存新圖片
             for img_file in request.FILES.getlist('images'):
@@ -3935,4 +3943,5 @@ def upload_store_ad(request):
         form = StoreAdForm()
 
     return render(request, 'store_upload_ad.html', {'form': form})
+
 
