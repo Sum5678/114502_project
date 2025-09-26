4bb89a898553edabd80d0eb4346030abeac3b983
11056001 Sunny <11056001@ntub.edu.tw>
2025-09-26 20:33:08 +0800

    改upload_store_ad

--- myapp/models.py
+++ myapp/models.py
@@ -442,6 +442,7 @@ class StoreAdHistory(models.Model):
         ('approved', '已批准'),
         ('rejected', '已拒絕'),
     ]
+    
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     reviewed_at = models.DateTimeField(blank=True, null=True)
